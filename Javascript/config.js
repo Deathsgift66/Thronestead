ff6d4ec8eb0fb010ae69091349d96f0b80c69ffd
Deathsgift66 <kingmakersrise@gmail.com>
2025-06-07 19:01:42 -0400

    Create config.js

---
+++ Javascript/config.js
@@ -0,0 +1,3 @@
+// config.js — for public keys (safe in browser)
+export const SUPABASE_URL = 'https://zzqoxgytfrbptojcwrjm.supabase.co';
+export const SUPABASE_ANON_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Inp6cW94Z3l0ZnJicHRvamN3cmptIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDcxMzQ5OTYsImV4cCI6MjA2MjcxMDk5Nn0.Tv_3Na5FFM35FI-p7vIlNj3yAo_WqkKAqLn3fqQ6k3Q';
