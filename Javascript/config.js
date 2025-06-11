db289da9f39c704820fdae94592c724da220cf61
Deathsgift66 <kingmakersrise@gmail.com>
2025-06-10 21:53:31 -0400

    Update config.js

--- Javascript/config.js
+++ Javascript/config.js
@@ -1,3 +1,3 @@
 // config.js — for public keys (safe in browser)
 export const SUPABASE_URL = 'https://zzqoxgytfrbptojcwrjm.supabase.co';
-export const SUPABASE_ANON_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Inp6cW94Z3l0ZnJicHRvamN3cmptIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDcxMzQ5OTYsImV4cCI6MjA2MjcxMDk5Nn0.Tv_3Na5FFM35FI-p7vIlNj3yAo_WqkKAqLn3fqQ6k3Q';
+export const SUPABASE_ANON_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Inp6cW94Z3l0ZnJicHRvamN3cmptIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDk1Nzk3MzYsImV4cCI6MjA2NTE1NTczNn0.mbFcI9V0ajn51SM68De5ox36VxbPEXK2WK978HZgUaE';
