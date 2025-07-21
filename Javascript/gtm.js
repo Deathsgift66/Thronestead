8ad4ee64bf7dcb92cf5840bc33d950f0ab91d029
Deathsgift66 <212032951+Deathsgift66@users.noreply.github.com>
2025-07-21 09:06:43 -0400

    feat: make gtm script configurable

--- Javascript/gtm.js
+++ Javascript/gtm.js
@@ -1,10 +1,18 @@
-(function(w, d, s, l, i) {
-  w[l] = w[l] || [];
-  w[l].push({ 'gtm.start': new Date().getTime(), event: 'gtm.js' });
-  var f = d.getElementsByTagName(s)[0],
-    j = d.createElement(s),
-    dl = l != 'dataLayer' ? '&l=' + l : '';
-  j.async = true;
-  j.src = 'https://www.googletagmanager.com/gtm.js?id=' + i + dl;
-  f.parentNode.insertBefore(j, f);
-})(window, document, 'script', 'dataLayer', 'GTM-XXXXXXX');
+import { getEnvVar } from './env.js';
+
+const GTM_ID = getEnvVar('GTM_ID');
+
+if (!GTM_ID) {
+  console.warn('⚠️ Missing Google Tag Manager ID.');
+} else {
+  (function (w, d, s, l, i) {
+    w[l] = w[l] || [];
+    w[l].push({ 'gtm.start': new Date().getTime(), event: 'gtm.js' });
+    const f = d.getElementsByTagName(s)[0];
+    const j = d.createElement(s);
+    const dl = l != 'dataLayer' ? '&l=' + l : '';
+    j.async = true;
+    j.src = 'https://www.googletagmanager.com/gtm.js?id=' + i + dl;
+    f.parentNode.insertBefore(j, f);
+  })(window, document, 'script', 'dataLayer', GTM_ID);
+}
