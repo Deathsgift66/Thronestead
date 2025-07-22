92c172888d8ac8038855ba17c1f25a81f9d57065
Deathsgift66 <212032951+Deathsgift66@users.noreply.github.com>
2025-07-22 14:14:20 -0400

    Bundle alliance scripts

---
+++ Javascript/allianceModules.js
@@ -0,0 +1,12 @@
+// Consolidated Alliance modules
+// This bundle combines all interactive alliance modules to reduce requests.
+import './allianceAppearance.js';
+import './requireAlliance.js';
+import './alliance_home.js';
+import './alliance_members.js';
+import './alliance_projects.js';
+import './alliance_quests.js';
+import './alliance_treaties.js';
+import './alliance_vault.js';
+import './alliance_wars.js';
+import './alliance_changelog.js';
