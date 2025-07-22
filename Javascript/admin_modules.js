6a2216d0853ccab62fabc12580ab41a31d2b5e8a
Deathsgift66 <212032951+Deathsgift66@users.noreply.github.com>
2025-07-22 14:49:50 -0400

    Fix admin pages imports and add admin meta

--- Javascript/admin_modules.js
+++ Javascript/admin_modules.js
@@ -1,4 +1,3 @@
-export * from './utils_admin.js';
 export { init as initAdminDashboard } from './admin_dashboard.js';
 export { init as initAdminAlerts } from './admin_alerts.js';
 export { init as initAdminEmergencyTools } from './admin_emergency_tools.js';
