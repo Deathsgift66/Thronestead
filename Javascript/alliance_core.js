45771ff8cec1c980f0101df75eb7b2e775935ddd
Deathsgift66 <212032951+Deathsgift66@users.noreply.github.com>
2025-07-22 14:30:35 -0400

    Add consolidated alliance_core.js

---
+++ Javascript/alliance_core.js
@@ -0,0 +1,24 @@
+// Consolidated alliance modules
+// This file aggregates multiple alliance-related scripts
+// so they can be imported with a single statement.
+// Modules not found in this repository are noted below.
+
+import './alliance_home.js';
+import './alliance_members.js';
+import './alliance_quests.js';
+import './alliance_projects.js';
+import './alliance_treaties.js';
+import './alliance_vault.js';
+import './alliance_wars.js';
+import './alliance_changelog.js';
+
+// The following modules were requested for consolidation but do not exist:
+// - alliance_diplomacy.js
+// - alliance_roles.js
+// - alliance_votes.js
+// - alliance_bank.js
+// - alliance_loans.js
+// - alliance_management.js
+// - alliance_achievements.js
+// If these modules are added in the future, import statements can be
+// appended here.
