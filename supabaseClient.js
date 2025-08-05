98abb199474c1ba6fe98a58be7f05f824b5436a2
Deathsgift66 <212032951+Deathsgift66@users.noreply.github.com>
2025-08-05 09:52:31 -0400

    chore: add project header to supabase client

--- supabaseClient.js
+++ supabaseClient.js
@@ -1,2 +1,5 @@
+// Project: Thronestead©
+// Subsystem: Supabase Client
+// Version: 0.1.2025.07.01
 // Deprecated: use /Javascript/utils_core.js instead
 export { supabase, supabaseReady } from './Javascript/utils_core.js';
